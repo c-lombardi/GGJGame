042b925aed7d21fe9928a20fdb508c639b5ef7c3
c-lombardi <clombardi@problemsolutions.net>
2015-01-24 19:36:15 -0500

    added enemy with random ai

---
+++ Assets/NavToPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavToPlayer : MonoBehaviour {
+	public Transform target;
+	NavMeshAgent nma;
+	// Use this for initialization
+	void Start () {
+		nma = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine(MyCoroutine ());
+
+	}
+
+	IEnumerator MyCoroutine()
+	{
+		//This is a coroutine
+		
+		yield return new WaitForSeconds(Random.Range(2, 5));
+		nma.SetDestination (Random.insideUnitSphere * Random.Range(10,100));
+	}
+}
