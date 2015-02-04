9075fc33db12600436ca79d3e32bb777e309c3ac
Light1c3 <fullmetala@walla.com>
2015-02-03 22:12:44 -0500

    Almost working menu

---
+++ Assets/LevelChange.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update(){
+
+	if(Input.GetKeyUp(KeyCode.Return)){
+		Application.LoadLevel ("GameOver");
+	}	
+
+}
---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyUp(KeyCode.Return)){
+			Application.LoadLevel ("GameOver");
+		}
+
+	}
+}
